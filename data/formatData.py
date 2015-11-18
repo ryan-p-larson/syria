672aa344e7dcafeedcae7c0cdf0e98f9c267682f
ryan-p-larson <ryalarson11@gmail.com>
2015-11-18 14:57:53 -0600

    Create data folder.

---
+++ data/formatData.py
@@ -0,0 +1,27 @@
+import csv
+
+refugees = {}
+f = open("monthly-data-uncleaned.csv")
+
+for row in csv.DictReader(f):
+    key = row["Country"]
+    refugees[key] = {}
+    refugees[key]["Country"] = key
+
+    for month in ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October"]:
+        value = row[month]
+        if value == " - ":
+            value = 0
+        
+        refugees[key][month] = value       
+
+with open("monthly-data.csv", "wb") as outfile:
+    attrNames = refugees["Canada"].keys()
+    print(attrNames)
+
+    writer = csv.DictWriter(outfile, fieldnames = attrNames)
+    writer.writeheader()
+
+    for country in refugees:
+        writer.writerow(refugees[country])
+
